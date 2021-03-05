e3e0249d416b2f9b91e036d5a87caa4467342aa2
samucastrov20 <sdcv.2000@gmail.com>
2021-03-04 19:42:12 -0600

    Create flake8_error.py
    Este commit agrega el código de python con tres errores detectables por flake 8.

---
+++ flake8_error.py
@@ -0,0 +1,8 @@
+def flake8_error(x):  # este comentario es muy largo para flake8 y por lo tanto hay error
+    if x == str(x):
+        return "Leisel"
+    if x<3:
+        return x
+    else:
+        return "Samuel"
+
